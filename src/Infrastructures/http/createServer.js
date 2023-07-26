edcba00cf8a3e0cea78c6bc21e2438be80bb9bad
Muhammad Naufal Farras <opal.farras@gmail.com>
2023-07-26 13:54:23 +0700

    Pass say Welcome test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -49,6 +49,14 @@ const createServer = async (container) => {
     },
   ]);
 
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      value: 'Welcome to Forum API',
+    }),
+  });
+
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
